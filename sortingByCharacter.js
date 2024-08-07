fb97a2950830b640ec0924ec05035252e2b3bc16
Pradit Yamsook <praditcry@gmail.com>
2024-08-07 20:04:58 +0700

    Update sortingByCharacter.js

--- sortingByCharacter.js
+++ sortingByCharacter.js
@@ -1,5 +1,4 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
+
 var objs = [
     {
         first_nom: '5.1.Laszlo', last_nom: [{ first_nom: '5.1.Laszlo', last_nom: 'Jamf' },
